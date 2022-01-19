3a67e00589b81c17e206683e0bafaed01c2ca378
Hercules Merscher <hlmerscher@gmail.com>
2022-01-19 23:53:29 +0100

    strings truncate at, left pad, right pad

--- strings/strings.go
+++ strings/strings.go
@@ -5,3 +5,34 @@ import "strings"
 func Lines(s string) []string {
 	return strings.Split(string(s), "\n")
 }
+
+func TruncateAt(value string, index int) string {
+	result := strings.Trim(value, " ")
+	if len(result) > index {
+		return result[:index]
+	}
+
+	return result
+}
+
+func LeftPad(value string, length int, char rune) string {
+	if length <= len(value) {
+		return value
+	}
+
+	repeat := length - len(value)
+	paddedVal := strings.Repeat(string(char), repeat) + value
+
+	return paddedVal
+}
+
+func RightPad(value string, length int, char rune) string {
+	if length <= len(value) {
+		return value
+	}
+
+	repeat := length - len(value)
+	paddedVal := value + strings.Repeat(string(char), repeat)
+
+	return paddedVal
+}
--- strings/strings_test.go
+++ strings/strings_test.go
@@ -8,3 +8,77 @@ func Test_strings_Lines(t *testing.T) {
 		t.Errorf("expected %d lines, got %d", 3, len(lines))
 	}
 }
+
+func Test_strings_TruncateAt(t *testing.T) {
+	t.Run("given an empty string", func(t *testing.T) {
+		if value := TruncateAt("", 10); value != "" {
+			t.Error("value should be empty")
+		}
+	})
+
+	t.Run("given a small string", func(t *testing.T) {
+		if value := TruncateAt("hi", 10); value != "hi" {
+			t.Error("value should be untouched")
+		}
+	})
+
+	t.Run("given a big string", func(t *testing.T) {
+		value := TruncateAt("Hochgeschwindigkeitszug", 10)
+		expected := "Hochgeschw"
+		if value != expected {
+			t.Errorf("expected %q, got %q", expected, value)
+		}
+	})
+}
+
+func Test_strings_LeftPad(t *testing.T) {
+	t.Run("given an empty string", func(t *testing.T) {
+		value := LeftPad("", 3, '0')
+		expected := "000"
+		if value != expected {
+			t.Errorf("expected %q, got %q", expected, value)
+		}
+	})
+
+	t.Run("given a string with equal size of padding", func(t *testing.T) {
+		value := LeftPad("hi", 2, 'A')
+		expected := "hi"
+		if value != expected {
+			t.Errorf("expected %q, got %q", expected, value)
+		}
+	})
+
+	t.Run("given a string that needs to be padded", func(t *testing.T) {
+		value := LeftPad("99", 5, '1')
+		expected := "11199"
+		if value != expected {
+			t.Errorf("expected %q, got %q", expected, value)
+		}
+	})
+}
+
+func Test_strings_RightPad(t *testing.T) {
+	t.Run("given an empty string", func(t *testing.T) {
+		value := RightPad("", 3, '0')
+		expected := "000"
+		if value != expected {
+			t.Errorf("expected %q, got %q", expected, value)
+		}
+	})
+
+	t.Run("given a string with equal size of padding", func(t *testing.T) {
+		value := RightPad("hi", 2, 'A')
+		expected := "hi"
+		if value != expected {
+			t.Errorf("expected %q, got %q", expected, value)
+		}
+	})
+
+	t.Run("given a string that needs to be padded", func(t *testing.T) {
+		value := RightPad("99", 5, '1')
+		expected := "99111"
+		if value != expected {
+			t.Errorf("expected %q, got %q", expected, value)
+		}
+	})
+}
